736f098a3556a519c6e6fd3909d8c89d0d0397a9
Priyanka Musti <priyamusti7@gmail.com>
2017-08-12 12:10:37 -0700

    Initial Commit

---
+++ ThisOrThatFINAL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThisOrThatFINAL
+//
+//  Created by Priyanka Musti on 8/12/17.
+//  Copyright © 2017 Priyanka Musti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
